23dc4cf6ac10209a22dcbabddd2bdb41ab194bae
Conor O'Dwyer <c9dw5er@protonmail.com>
2020-02-04 23:49:13 +0000

    Navigation Bar Styling

---
+++ FindingTaylorSwift/FindViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FindViewController.swift
+//  FindingTaylorSwift
+//
+//  Created by The App Experts on 04/02/2020.
+//  Copyright © 2020 Conor O'Dwyer. All rights reserved.
+//
+
+import UIKit
+
+class FindViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let attributes = [NSAttributedString.Key.foregroundColor : UIColor.white, NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17)]
+        self.navigationController?.navigationBar.titleTextAttributes = attributes
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.barTintColor = #colorLiteral(red: 1, green: 0.738589704, blue: 0.9438112974, alpha: 1)
+        self.navigationItem.title = "FINDING TAYLOR SWIFT"
+        
+        self.navigationItem.rightBarButtonItem?.tintColor = .white
+        self.navigationItem.leftBarButtonItem?.tintColor = .white
+    }
+       
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}

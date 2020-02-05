ddb56447e19f177659ce432a7fcdbe2a2219d394
Conor O'Dwyer <c9dw5er@protonmail.com>
2020-02-05 17:24:20 +0000

    The User Selects An Image
    A user can select an image from the camera or
    image library, edit and crop it

--- FindingTaylorSwift/FindViewController.swift
+++ FindingTaylorSwift/FindViewController.swift
@@ -7,8 +7,18 @@
 //
 
 import UIKit
+import CropViewController
 
-class FindViewController: UIViewController {
+class FindViewController: UIViewController, CropViewControllerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var selectImage: UIBarButtonItem!
+    @IBOutlet var uploadSelectedImage: UIBarButtonItem!
+    @IBOutlet var imageView: UIImageView!
+    private var image: UIImage?
+    
+    private var croppingStyle = CropViewCroppingStyle.default
+    private var croppedRect = CGRect.zero
+    private var croppedAngle = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,18 +31,138 @@ class FindViewController: UIViewController {
         
         self.navigationItem.rightBarButtonItem?.tintColor = .white
         self.navigationItem.leftBarButtonItem?.tintColor = .white
+        
+        navigationItem.rightBarButtonItem?.isEnabled = false
+        imageView.isUserInteractionEnabled = true
+        self.imageView.contentMode = .scaleAspectFill
+        self.imageView.clipsToBounds = true
+        
+        if #available(iOS 11.0, *) {
+            imageView.accessibilityIgnoresInvertColors = true
+        }
+        view.addSubview(imageView)
+        
+        let tapRecognizer = UITapGestureRecognizer.init(target: self, action: #selector(didTapImageView))
+        imageView.addGestureRecognizer(tapRecognizer)
+        
+        
     }
        
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = (info[UIImagePickerController.InfoKey.originalImage] as? UIImage) else { return }
+        
+        let cropController = CropViewController(croppingStyle: croppingStyle, image: image)
+        cropController.delegate = self
+        
+        self.image = image
+        
+        //If profile picture, push onto the same navigation stack
+        if croppingStyle == .circular {
+            if picker.sourceType == .camera {
+                picker.dismiss(animated: true, completion: {
+                    self.present(cropController, animated: true, completion: nil)
+                })
+            } else {
+                picker.pushViewController(cropController, animated: true)
+            }
+        }
+        else { //otherwise dismiss, and then present from the main controller
+            picker.dismiss(animated: true, completion: {
+                self.present(cropController, animated: true, completion: nil)
+            })
+        }
+    }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    public func cropViewController(_ cropViewController: CropViewController, didCropToImage image: UIImage, withRect cropRect: CGRect, angle: Int) {
+        self.croppedRect = cropRect
+        self.croppedAngle = angle
+        updateImageViewWithImage(image, fromCropViewController: cropViewController)
+    }
     
+    public func cropViewController(_ cropViewController: CropViewController, didCropToCircularImage image: UIImage, withRect cropRect: CGRect, angle: Int) {
+        self.croppedRect = cropRect
+        self.croppedAngle = angle
+        updateImageViewWithImage(image, fromCropViewController: cropViewController)
+    }
+    
+    private func updateImageViewWithImage(_ image: UIImage, fromCropViewController cropViewController: CropViewController) {
+        imageView.image = image
+        layoutImageView()
+        
+        self.navigationItem.rightBarButtonItem?.isEnabled = true
+        
+        if cropViewController.croppingStyle != .circular {
+            imageView.isHidden = true
+            
+            cropViewController.dismissAnimatedFrom(self, withCroppedImage: image,
+                                                   toView: imageView,
+                                                   toFrame: CGRect.zero,
+                                                   setup: { self.layoutImageView() },
+                                                   completion: {
+                                                    self.imageView.isHidden = false })
+        }
+        else {
+            self.imageView.isHidden = false
+            cropViewController.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    @objc public func didTapImageView() {
+        // When tapping the image view, restore the image to the previous cropping state
+        let cropViewController = CropViewController(croppingStyle: self.croppingStyle, image: self.image!)
+        cropViewController.delegate = self
+        let viewFrame = view.convert(imageView.frame, to: navigationController!.view)
+        
+        cropViewController.presentAnimatedFrom(self,
+                                               fromImage: self.imageView.image,
+                                               fromView: nil,
+                                               fromFrame: viewFrame,
+                                               angle: self.croppedAngle,
+                                               toImageFrame: self.croppedRect,
+                                               setup: { self.imageView.isHidden = true },
+                                               completion: nil)
+    }
+    
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        layoutImageView()
+    }
+    
+    private func layoutImageView() {
+        guard imageView.image != nil else { return }
+        
+        let margins = view.layoutMarginsGuide
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 5.0).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: -5.0).isActive = true
+        imageView.topAnchor.constraint(equalTo: margins.topAnchor, constant: 5.0).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: -5.0).isActive = true
+        view.addSubview(imageView)
+    }
+    
+    @IBAction func clickSelectImage(_ sender: UIBarButtonItem) {
+        
+        self.croppingStyle = .default
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        imagePicker.delegate = self
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func clickUploadSelectedImage(_ sender: UIBarButtonItem) {
+        
+        guard let image = imageView.image else {
+            return
+        }
+        
+        navigationItem.title = "Uploading Image..."
+        //awsFilehandle.uploadFile(withImage: image, bucketName: "match-image")
+    }
 }

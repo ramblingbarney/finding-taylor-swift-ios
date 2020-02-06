d79eaa728ce77952b0fd354d81d0b0d956b68c26
Conor O'Dwyer <c9dw5er@protonmail.com>
2020-02-06 10:46:44 +0000

    Tagged Photos Of Taylor Swift Or Clothes

--- FindingTaylorSwift/ViewControllers/FindViewController.swift
+++ FindingTaylorSwift/ViewControllers/FindViewController.swift
@@ -30,7 +30,7 @@ class FindViewController: UIViewController, CropViewControllerDelegate, UIImageP
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let attributes = [NSAttributedString.Key.foregroundColor : UIColor.white, NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17)]
+        let attributes = [NSAttributedString.Key.foregroundColor : UIColor.white, NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 17)]
         self.navigationController?.navigationBar.titleTextAttributes = attributes
         self.navigationController?.navigationBar.isTranslucent = false
         self.navigationController?.navigationBar.barTintColor = #colorLiteral(red: 1, green: 0.738589704, blue: 0.9438112974, alpha: 1)
